a4873dbf60f887782793a1d43dcdf3d0e435d200
reece-vlr <reece.vlr@gmail.com>
2023-05-07 16:36:05 +0800

    Add repeatString sol'n

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let repeat = '';
 
+    if (num < 0) {
+        repeat = 'ERROR';
+    }
+
+    for (let i = 0; i < num; i++) {
+        repeat += str;
+    }
+    return repeat;
 };
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
