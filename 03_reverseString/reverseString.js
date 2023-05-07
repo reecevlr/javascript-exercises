1d26a010542315c74cd29a3d1fd942fe41bb92cd
reece-vlr <reece.vlr@gmail.com>
2023-05-07 16:48:32 +0800

    Add reverseString sol'n

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // split
+    // reverse
+    // join
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
