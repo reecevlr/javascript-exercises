32555fbd246a1267a47162c6dbf4275e73e2a1f2
reece-vlr <reece.vlr@gmail.com>
2023-05-08 15:34:39 +0800

    Add sumAll sol'n

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    // Validity check (non-number || negative)
+    if (typeof startNum !== 'number' || 
+        typeof endNum !== 'number' || 
+        startNum < 0 || 
+        endNum < 0) {
+        return 'ERROR'
+    }
+
+    if (startNum < endNum) {
+        for (; startNum <= endNum; startNum++) {
+            sum += startNum;
+        }
+    } 
+    else {
+        for (; endNum <= startNum; endNum++) {
+            sum += endNum;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
