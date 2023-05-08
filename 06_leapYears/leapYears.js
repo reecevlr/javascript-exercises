f311c1830a44bc0078d9e5f8bd8ff7913e5b4f34
reece-vlr <reece.vlr@gmail.com>
2023-05-08 15:49:31 +0800

    Add leapYears sol'n

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    }
+    else if (year % 4 === 0 &&
+             year % 100 === 0 && 
+             year % 400 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
+    /*
+        Since OR returns first truthy value, 
+        if year is !divisible by 100, it returns true. Else,
+        it checks if it is divisible by 400, if it is, leapYear. Else,
+        !leapYear.
+        
+        return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    */
 };
 
 // Do not edit below this line
