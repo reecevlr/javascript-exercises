0e8bc4d7027e7937eb343c369f57d0c678df0710
reece-vlr <reece.vlr@gmail.com>
2023-05-08 16:09:48 +0800

    Add tempConversion sol'n

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result = (fahrenheit - 32) * (5/9);
+  return round(result, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = (celsius * (9 / 5)) + 32;
+  return round(result, 1);
 };
+
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
