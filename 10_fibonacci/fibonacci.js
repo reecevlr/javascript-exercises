017d269f7ccbfbb43830de89ce760b7c9383f9f8
reece-vlr <reece.vlr@gmail.com>
2023-07-04 21:12:13 +0800

    Add soln

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    }
+    
+    let x = 1,
+        y = 1,
+        z;
 
+    if (num <= 2) {
+        return 1;
+    }
+
+    for (let i = 2; i < num; i++) {
+        z = x + y;
+        x = y;
+        y = z;
+    }
+    return z;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currYear = new Date().getFullYear();
 
+    const newArr = arr.sort((a, b) => {
+        if (typeof a.yearOfDeath === 'undefined') {
+            a.yearOfDeath = currYear;
+        } 
+        if (typeof b.yearOfDeath == 'undefined') {
+            b.yearOfDeath = currYear;
+        }
+
+        let ageA = a.yearOfDeath - a.yearOfBirth;
+        let ageB = b.yearOfDeath - b.yearOfBirth;
+
+        return ageA > ageB ? -1 : 1;
+    });
+    return newArr.shift();
 };
 
 // Do not edit below this line
