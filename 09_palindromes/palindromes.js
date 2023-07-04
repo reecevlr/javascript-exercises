2ecb346aa4c13cd0256cd05ceecfcfbd3ac05a74
reece-vlr <reece.vlr@gmail.com>
2023-07-04 17:08:41 +0800

    Add soln

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+// https://www.freecodecamp.org/news/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7/
+const palindromes = function (str) {
+    let re = /[\W_]/g;
 
+    let lowRe = str.toLowerCase().replace(re, '');
+    let reverse = lowRe.split('').reverse().join('');
+
+    return lowRe === reverse;
 };
 
 // Do not edit below this line
